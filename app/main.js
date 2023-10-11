54e886c91b9b58913cb777cb783fd027f54fa84d
Elvira Magasumova <mova.es94@gmail.com>
2023-10-11 22:03:25 +0500

    lesson two homework done

---
+++ app/main.js
@@ -0,0 +1,9 @@
+import "./ui/style.pcss"
+import { runApp } from "../shared/api/App.js"
+
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+  </div>
+`
---
+++ shared/api/App.js
@@ -0,0 +1,17 @@
+export const runApp = async () => {
+    switch (process.env.NODE_ENV) {
+        case "development":
+            await import("./browser.js")
+                .then(async ({ worker }) => {
+                    await worker.start().then(() => {
+                        console.debug("App dev run")
+                    })
+                })
+
+    }
+}
+
+runApp()
+    .catch((err) => {
+        console.error(err)
+    })
